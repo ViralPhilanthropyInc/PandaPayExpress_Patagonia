e351a37066a7f2e121433794e29f55cb436f2374
dalton-cole <dalton@rocketpartners.io>
2018-01-18 00:57:40 -0500

    most recent changes to patagonia's PPE implementation

--- pp-express.js
+++ pp-express.js
@@ -1,5 +1,53 @@
+function successfulTransaction(data) {
+    window.location = "https://s3.amazonaws.com/express.pandapay.io/patagonia-dev/pandapay-express-iframe-thank-you.html?charityName=" + getAllUrlParams().charityName + '&patagoniaUserID=' + getAllUrlParams().patagoniaUserID + '&cyberGrantsID=' + getAllUrlParams().cyberGrantsID + '&issues=' + getAllUrlParams().issues + '&wordpressID=' + getAllUrlParams().wordpressID;
+};
+
+var closeButton = document.getElementsByClassName("close")[0];
+closeButton.onclick = function () {
+    window.parent.postMessage("close", "*");
+};
+
+function getAllUrlParams(url) {
+    var queryString = url ? url.split('?')[1] : window.location.search.slice(1);
+    var obj = {};
+    if (queryString) {
+        queryString = queryString.split('#')[0];
+        var arr = queryString.split('&');
+        for (var i = 0; i < arr.length; i++) {
+            var a = arr[i].split('=');
+            var paramNum = undefined;
+            var paramName = a[0].replace(/\[\d*\]/, function (v) {
+                paramNum = v.slice(1, -1);
+                return '';
+            });
+            var paramValue = typeof (a[1]) === 'undefined' ? true : a[1];
+            if (obj[paramName]) {
+                if (typeof obj[paramName] === 'string') {
+                    obj[paramName] = [obj[paramName]];
+                }
+                if (typeof paramNum === 'undefined') {
+                    obj[paramName].push(paramValue);
+                }
+                else {
+                    obj[paramName][paramNum] = paramValue;
+                }
+            }
+            else {
+                obj[paramName] = paramValue;
+            }
+        }
+    }
+    return obj;
+}
+
+var granteeNames = document.getElementsByClassName("granteeName"),
+    i, len;
+for (i = 0, len = granteeNames.length; i < len; i++) {
+    granteeNames[i].innerHTML = decodeURI(getAllUrlParams().charityName) + " ";
+}
+
 var ppCharityName = localStorage.getItem('pandapayCharityName');
-var ppEin = localStorage.getItem('pandapayCharityEIN');
+var ppEin = decodeURI(getAllUrlParams().charityEIN);
 var ppFirstName = localStorage.getItem('pandapayFirstName');
 var ppLastName = localStorage.getItem('pandapayLastName');
 var ppAmount = localStorage.getItem('pandapayDonationAmount');
@@ -7,18 +55,17 @@ var ppReceiptEmail = localStorage.getItem('pandapayReceiptEmail');
 var ppCardType = localStorage.getItem('pandapayCardType');
 var ppLastFour = localStorage.getItem('pandapayLastFour');
 
-function successfulTransaction(data) {
-  	window.location = "https://s3.amazonaws.com/express.pandapay.io/patagonia-dev/pandapay-express-iframe-thank-you.html";
-    };   
+(function (i, s, o, g, r, a, m) {
+i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function () {
+    (i[r].q = i[r].q || []).push(arguments)
+}, i[r].l = 1 * new Date(); a = s.createElement(o),
+    m = s.getElementsByTagName(o)[0]; a.async = 1; a.src = g; m.parentNode.insertBefore(a, m)
+})(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
 
-// var closeButton = document.getElementsByClassName("close")[0];
-//	closeButton.onclick = function() {
-//		parent.remoteCloseModal();   
-//	};
-
-var granteeNames = document.getElementsByClassName("granteeName"),
-    i, len;
-for (i = 0, len = granteeNames.length; i < len; i++) {
-    granteeNames[i].innerHTML = ppCharityName + " ";
-}
-
+ga('create', 'UA-104969319-1', 'auto');
+ga('set', 'userId', decodeURI(getAllUrlParams().patagoniaUserID));
+ga('set', 'dimension1', decodeURI(getAllUrlParams().cyberGrantsID));
+ga('set', 'dimension2', 'ngo');
+ga('set', 'dimension3', decodeURI(getAllUrlParams().issues));
+ga('set', 'dimension4', decodeURI(getAllUrlParams().wordpressID));
+ga('send', 'pageview');
--- pp_express.js
+++ pp_express.js
@@ -9,15 +9,21 @@ ppModal.style.width = "100%";
 ppModal.style.height = "100%";
 ppModal.style.overflow = "auto";
 ppModal.style.backgroundColor = "rgba(0,0,0,0.8)";
-ppModal.innerHTML = "<div class=\"pp-donation-modal-content\" style=\"background-color: #fefefe; margin: 10% auto; padding: 0px; border: 0px; width: 440px;\"><iframe src=\"\" height=\"350px\" width=\"100%\" seamless='seamless' frameBorder=\"0\" id=\"pp-donation-iframe\"></iframe></div>"
+ppModal.innerHTML = "<div class=\"pp-donation-modal-content\" style=\"background-color: #fefefe; margin: 10% auto; padding: 0px; border: 0px; width: 100%; max-width:440px;\"><iframe src=\"\" height=\"480px\" width=\"100%\" seamless='seamless' frameBorder=\"0\" id=\"pp-donation-iframe\"></iframe></div>"
 var ppiframe = document.getElementById("pp-donation-iframe")
 function closeModal() { ppModal.style.display = "none"; }
-function loadPPModal(charityName, charityEIN, firstName, lastName) {
-	localStorage.setItem('pandapayCharityName', charityName);
-	localStorage.setItem('pandapayCharityEIN', charityEIN);
-	localStorage.setItem('pandapayFirstName', firstName);
-	localStorage.setItem('pandapayLastName', lastName);
-	ppiframe.src = 'https://s3.amazonaws.com/express.pandapay.io/patagonia-dev/pandapay-express-iframe.html'; ppModal.style.display = "block";
+function loadPPModal(charityName, charityEIN, firstName, lastName, id, cyberGrantsID, issues, customerNo) {
+	iFrameURL = 'https://s3.amazonaws.com/express.pandapay.io/patagonia-dev/pandapay-express-iframe.html?charityName='+ charityName + '&charityEIN=' + charityEIN + '&firstName=' + firstName + '&lastName=' + lastName + '&patagoniaUserID=' + customerNo + '&cyberGrantsID=' + cyberGrantsID + '&issues=' + issues + '&wordpressID=' + id
+	ppiframe.src = encodeURI(iFrameURL) ; ppModal.style.display = "block";
 }
 window.onclick = function (event) { if (event.target == ppModal) { closeModal(); } }
-var remoteCloseModal = closeModal();
+
+window.addEventListener('message', function(event) {  
+    if (~event.origin.indexOf('https://express.pandpay.io')) { 
+		if (event.data == "close") {
+			closeModal();
+		}
+    } else { 
+        return; 
+    } 
+}); 
